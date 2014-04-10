c53ae2a5a4ebd69093282b80a494be8591f920bf
Anthony Gelibert <anthony.gelibert@me.com>
2014-04-10 10:30:59 +0200

    Rename method.

--- src/main/java/fr/gelibert/tests/choco/IntroductiveExample.java
+++ src/main/java/fr/gelibert/tests/choco/IntroductiveExample.java
@@ -16,7 +16,7 @@ import java.util.Calendar;
  * @version 1.0.1
  */
 public final class IntroductiveExample extends AbstractProblem {
-    IntVar[] all = new IntVar[5];
+    IntVar[] m_allVars = new IntVar[5];
 
     @Override
     public void createSolver() {
@@ -26,23 +26,23 @@ public final class IntroductiveExample extends AbstractProblem {
     @Override
     public void buildModel() {
         // Variables
-        all[0] = VariableFactory.enumerated("Xa", new int[]{1, 2}, solver);/* NON-NLS */
-        all[1] = VariableFactory.enumerated("Xb", new int[]{1, 2, 3, 4, 5}, solver);/* NON-NLS */
-        all[2] = VariableFactory.enumerated("Xc", new int[]{1, 2, 3}, solver);/* NON-NLS */
-        all[3] = VariableFactory.enumerated("Xd", new int[]{1, 2, 3, 4, 5}, solver);/* NON-NLS */
-        all[4] = VariableFactory.enumerated("d", new int[]{1, 2}, solver);/* NON-NLS */
+        final IntVar xa = VariableFactory.enumerated("Xa", new int[]{1, 2}, solver);/* NON-NLS */
+        final IntVar xb = VariableFactory.enumerated("Xb", new int[]{1, 2, 3, 4, 5}, solver);/* NON-NLS */
+        final IntVar xc = VariableFactory.enumerated("Xc", new int[]{1, 2, 3}, solver);/* NON-NLS */
+        final IntVar xd = VariableFactory.enumerated("Xd", new int[]{1, 2, 3, 4, 5}, solver);/* NON-NLS */
+        final IntVar d = VariableFactory.enumerated("d", new int[]{1, 2}, solver);/* NON-NLS */
 
-
+        m_allVars = new IntVar[]{xa,xb,xc,xd,d};
         // Constraints
-        solver.post(IntConstraintFactory.alldifferent(new IntVar[]{all[0], all[1], all[2], all[3]}, "BC"));
-        solver.post(IntConstraintFactory.arithm(all[2], "<", all[3]));
-        solver.post(IntConstraintFactory.scalar(new IntVar[]{all[1], all[4], all[3]}, new int[]{1, 1, -1}, "=", VariableFactory.fixed(1, solver)));
+        solver.post(IntConstraintFactory.alldifferent(new IntVar[]{xa, xb, xc, xd}, "BC")); /* NON-NLS */
+        solver.post(IntConstraintFactory.arithm(xc, "<", xd));
+        solver.post(IntConstraintFactory.scalar(new IntVar[]{xb, d, xd}, new int[]{1, 1, -1}, "=", VariableFactory.fixed(1, solver)));
     }
 
     @Override
     public void configureSearch() {
-        //solver.set(IntStrategyFactory.domOverWDeg_InDomainMin(all, 0));
-        solver.set(IntStrategyFactory.ImpactBased(all, Calendar.getInstance().getTimeInMillis()));
+        //solver.set(IntStrategyFactory.domOverWDeg_InDomainMin(m_allVars, 0));
+        solver.set(IntStrategyFactory.ImpactBased(m_allVars, Calendar.getInstance().getTimeInMillis()));
     }
 
     @Override
